e9c5a4b07423934fca7ab740b7bac068df2b7c4b
DoctorKrill08 <doctorkrill08@gmail.com>
2025-11-04 00:48:23 -0500

    Modified Drive only to not DC when expansion hub disconnected

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveOnly.java
@@ -30,7 +30,7 @@ public class DriveOnly extends LinearOpMode {
         waitForStart();
         if (isStopRequested()) return;
 
-        Conveyor conveyor = new Conveyor();
+      /*  Conveyor conveyor = new Conveyor();
         conveyor.initiate(hardwareMap);
         Flywheel flywheel = new Flywheel();
         flywheel.initiate(hardwareMap);
@@ -38,10 +38,12 @@ public class DriveOnly extends LinearOpMode {
         trigger.initiate(hardwareMap);
         Intake intake = new Intake();
         intake.initiate(hardwareMap);
-        Drivetrain drivetrain = new Drivetrain();
-        drivetrain.initiate(hardwareMap);
         Kicker kicker = new Kicker();
         kicker.initiate(hardwareMap);
+
+       */
+        Drivetrain drivetrain = new Drivetrain();
+        drivetrain.initiate(hardwareMap);
         Gamepad previousGamepad1 = new Gamepad();
         while (opModeIsActive()) {
             boolean LB = gamepad1.left_bumper && !previousGamepad1.left_bumper;
@@ -50,7 +52,7 @@ public class DriveOnly extends LinearOpMode {
             boolean LT = gamepad1.left_trigger > 0.1 && previousGamepad1.left_trigger <= 0.1;
             boolean RT = gamepad1.right_trigger > 0.1 && previousGamepad1.right_trigger <= 0.1;
             previousGamepad1.copy(gamepad1);
-
+/*
             switch (currentState) {
                 case RESTING:
                     if (RT) {
@@ -142,6 +144,8 @@ public class DriveOnly extends LinearOpMode {
                     flywheel.setState(Flywheel.States.SPINNING);
                 }
             }
+
+ */
 
             drivetrain.update(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
             telemetry.addData("State", currentState);
